c49cb27581d21071efea44ebefbc9a667307856c
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-05 02:05:40 +0800

    Added barter settings file

---
+++ main/barter-settings.js
@@ -0,0 +1,96 @@
+var barterSettings = {
+	"DATA": {
+
+	},
+	"DEFAULT": {
+
+	},
+	"FEATURES": {
+		/*
+			Each feature is an array of sub features.
+			The features are divided into these sections.
+			A feature can only do the following:
+				1. Replace default features.
+				2. Add to default features.
+				3. Modify default features.
+
+			Note that replacing and modifying default features
+				requires extreme consideration before deploying.
+
+			Each feature can affect the following:
+				1. Directives.
+				2. Controllers.
+				3. Styles.
+				4. Services.
+				5. Factories.
+				6. Templates.
+				7. Providers.
+
+			Format:
+				{
+					"name": "feature name",
+					"author": "feature author name",
+					"action": "add|replace|modify",
+					"directives": [
+						{
+							"name": "the same name you assign to your directive script",
+							"location": "url|path",
+							"dependencies": [ 
+								"list of dependencies",
+								{
+									"name": "this will be used by requirejs"
+									"location": "url|path"
+								}
+							]
+						}
+					],
+					"controllers": [
+						{
+							"name": "the same name you assign to your controller script",
+							"location": "url|path",
+							"dependencies": [ "list of dependencies" ]
+						}
+					],
+					"styles": [
+						{
+							"name": "this name will be used on the require part",
+							"location": "url|path",
+							"dependencies": [ "list of dependencies" ]
+						}
+					],
+					"services": [
+						{
+							"name": "the same name you assign to your service script",
+							"location": "url|path",
+							"dependencies": [ "list of dependencies" ]
+						}
+					],
+					"factories": [
+						{
+							"name": "the same name you assign to your factory script",
+							"location": "url|path",
+							"dependencies": [ "list of dependencies" ]
+						}
+					],
+					"templates": [
+						{
+							"name": "the same name you assign to your service script",
+							"location": "url|path",
+						}
+					],
+					"providers": [
+						{
+							"name": "the same name you assign to your provider script",
+							"location": "url|path",
+							"dependencies": [ "list of dependencies" ]
+						}
+					]
+				}
+		*/
+		"barterHeader": [ ],
+		"barterBody": [ ],
+		"barterFooter": [ ],
+		"barterContent": [ ],
+		"barterFunction": [ ]
+	}
+};

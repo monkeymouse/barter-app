24200f081811ede49a708c6609e1d84da34b8228
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-14 02:38:51 +0800

    Update code for server development.

---
+++ barter-node-server.js
@@ -0,0 +1,13 @@
+var http = require( "http" );
+
+var barterServer = http.createServer( );
+
+barterServer.on( "request",
+	function( request, response ){
+		response.writeHead( 200, {
+			"Content-Type": "text/plain"
+		} );
+		response.end( "Hello World", "utf8" );
+	} );
+
+barterServer.listen( 8080, "192.168.1.123" );

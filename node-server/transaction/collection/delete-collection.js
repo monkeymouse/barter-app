33e601e9c1126d70352614a8722b322879b2502c
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-10-23 20:38:07 +0800

    Made some code changes.

---
+++ node-server/transaction/collection/delete-collection.js
@@ -0,0 +1,20 @@
+var barterDb = require( "../../../barter-db.js" );
+
+exports.deleteCollection = function deleteCollection( collectionName, callback ){
+	barterDb.countData( collectionName, { "type": "schema" },
+		function( error, count ){
+			if( error ){
+				callback( error );
+				return;
+			}
+			if( count == 1 ){
+				barterDb.deleteData( collectionName, { },
+					function( error, count ){
+						callback( error, count == 1 );
+					} );
+				//TODO: Fire a delete on collection.
+			}else if( count == 0 ){
+				callback( null, true );
+			}
+		} );
+};
